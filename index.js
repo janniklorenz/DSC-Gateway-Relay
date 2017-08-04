b72c8d09160e91e57ecbef838c288a2b81c021e0
Jannik Lorenz <dev@janniklorenz.de>
2017-08-04 19:39:47 +0200

    fix start timing

--- index.js
+++ index.js
@@ -17,48 +17,51 @@ const config = require("./config/");
 
 // Start Main Server Processes
 var socketServerProcess = child_process.fork("./lib/SocketServer.js");
+socketServerProcess.on("message", function(event){
 
+  // -------- Client Socket --------
+  var clientSocket = SocketIOClient(config.relay.dscGatewayServer);
+
+  clientSocket.on('connect', function(){
+    console.info("DSC-Gateway client socket connected");
+  });
+  clientSocket.on('disconnect', function(){
+    console.info("DSC-Gateway client socket disconnected");
+    socketServerProcess.send({
+      type: "disconnect",
+    });
+  });
+
+  clientSocket.on("onlineLines", function(data){
+    console.log("on onlineLines")
+    socketServerProcess.send({
+      type: "onlineLines",
+      data: data,
+    });
+  });
+  clientSocket.on("setConfig", function(data){
+    console.log("on setConfig")
+    socketServerProcess.send({
+      type: "setConfig",
+      data: data,
+    });
+  });
+  clientSocket.on("setData", function(data){
+    console.log("on setData")
+    socketServerProcess.send({
+      type: "setData",
+      data: data,
+    });
+  });
+  clientSocket.on("setTeam", function(data){
+    console.log("on setTeam")
+    socketServerProcess.send({
+      type: "setTeam",
+      data: data,
+    });
+  });
+});
 socketServerProcess.on("exit", function(){
   console.error("[Main Process] Socket Server Worker did exit, stopping DSC...");
   process.exit();
-});
-
-
-
-// -------- Client Socket --------
-var clientSocket = SocketIOClient(config.relay.dscGatewayServer);
-
-clientSocket.on('connect', function(){
-  console.info("DSC-Gateway client socket connected");
-});
-clientSocket.on('disconnect', function(){
-  console.info("DSC-Gateway client socket disconnected");
-  socketServerProcess.send({
-    type: "disconnect",
-  });
-});
-
-clientSocket.on("onlineLines", function(data){
-  socketServerProcess.send({
-    type: "onlineLines",
-    data: data,
-  });
-});
-clientSocket.on("setConfig", function(data){
-  socketServerProcess.send({
-    type: "setConfig",
-    data: data,
-  });
-});
-clientSocket.on("setData", function(data){
-  socketServerProcess.send({
-    type: "setData",
-    data: data,
-  });
-});
-clientSocket.on("setTeam", function(data){
-  socketServerProcess.send({
-    type: "setTeam",
-    data: data,
-  });
 });
